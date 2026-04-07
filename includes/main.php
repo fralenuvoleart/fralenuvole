8d418cce4bb07b074529412b349b2bfb67c6b591
Fralenuvole Developer <dev@fralenuvole.art>
2026-04-08 00:43:57 +0800

    bug fix

--- includes/main.php
+++ includes/main.php
@@ -244,6 +244,37 @@ function frl_disable_oembed()
 }
 
 /**
+ * Filter function used to remove the tinymce emoji plugin.
+ * @param array $plugins
+ * @return array
+ */
+function frl_disable_emojis_tinymce($plugins)
+{
+    if (frl_is_array_not_empty($plugins)) {
+        return array_diff($plugins, array('wpemoji'));
+    } else {
+        return array();
+    }
+}
+
+/**
+ * Remove emoji CDN hostname from DNS prefetching hints.
+ * @param array $urls URLs to print for resource hints.
+ * @param string $relation_type The relation type the URLs are printed for.
+ * @return array
+ */
+function frl_disable_emojis_remove_dns_prefetch($urls, $relation_type)
+{
+    if ('dns-prefetch' == $relation_type) {
+        /** This filter is documented in wp-includes/formatting.php */
+        $emoji_svg_url = apply_filters('emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/');
+
+        $urls = array_diff($urls, array($emoji_svg_url));
+    }
+    return $urls;
+}
+
+/**
  * Disable emojis functionality
  *
  * @return void
--- public/public.php
+++ public/public.php
@@ -344,36 +344,6 @@ function frl_remove_jquery_migrate($scripts)
     }
 }
 
-/**
- * Filter function used to remove the tinymce emoji plugin.
- * @param array $plugins
- * @return array Difference betwen the two arrays
- */
-function frl_disable_emojis_tinymce($plugins)
-{
-    if (frl_is_array_not_empty($plugins)) {
-        return array_diff($plugins, array('wpemoji'));
-    } else {
-        return array();
-    }
-}
-
-/**
- * Remove emoji CDN hostname from DNS prefetching hints.
- * @param array $urls URLs to print for resource hints.
- * @param string $relation_type The relation type the URLs are printed for.
- * @return array Difference betwen the two arrays.
- */
-function frl_disable_emojis_remove_dns_prefetch($urls, $relation_type)
-{
-    if ('dns-prefetch' == $relation_type) {
-        /** This filter is documented in wp-includes/formatting.php */
-        $emoji_svg_url = apply_filters('emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/');
-
-        $urls = array_diff($urls, array($emoji_svg_url));
-    }
-    return $urls;
-}
 
 /**
  * Disable some REST endpoints for unauthenticated users
