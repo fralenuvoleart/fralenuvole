db3af8f761d43cc1b0bf018bf85bcb0838203a38
Fralenuvole Developer <dev@fralenuvole.art>
2026-04-21 09:06:03 +0800

    edit

--- config/config-options.php
+++ config/config-options.php
@@ -784,7 +784,7 @@ const FRL_DEFAULT_FIELDS = array(
 		),
 	),
 	'integrations' => array(
-		'title' => 'Modules and Integrations',
+		'title' => 'Modules & Integrations',
 		'fields' => array(
 			'section_title_plugin_exclusion' => array(
 				'label' => 'Thirdparty Plugin Exclusions',
