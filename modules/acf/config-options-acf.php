148927c233d7c8e6020a140ead5d0c0d7956a83f
Fralenuvole Developer <dev@fralenuvole.art>
2026-07-09 09:42:08 +0800

    Edit

--- modules/acf/config-options-acf.php
+++ modules/acf/config-options-acf.php
@@ -9,17 +9,3 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- * Add plugin options to the modules tab
- *
- * @param array $fields Existing settings fields
- * @return array Modified settings fields
- */
-$frl_acf_default_fields = array(
-	// Add a section title to the modules tab
-	'section_title_acf' => array(
-		'label'       => 'ACF Custom Fields Module',
-		'type'        => 'section_title',
-		'description' => 'ACF Custom Fields module settings',
-	),
-);
