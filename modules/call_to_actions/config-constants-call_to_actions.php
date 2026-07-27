6bb4da8249eb8ee89241ac4c8eb5b8f4ce3b76cd
Fralenuvole Developer <dev@fralenuvole.art>
2026-07-27 16:01:58 +0800

    CTA Message

--- modules/call_to_actions/config-constants-call_to_actions.php
+++ modules/call_to_actions/config-constants-call_to_actions.php
@@ -35,7 +35,7 @@ const CTA_WEBHOOK_CONFIG = array(
 			array(
 				'action_id'    => 'whatsapp',
 				'url'          => 'https://wa.me/995599654454?text={template}',
-				'template'     => "Support number: PIN-{reference_id}-PBS\r\n---\r\nHello, please type your message below.{br}{br}",
+				'template'     => "Support number: PIN-{reference_id}-PBS (please do not delete)\r\n---\r\nHello, please type your message below.{br}{br}",
 				'send_webhook' => true,
 			),
 			array(
